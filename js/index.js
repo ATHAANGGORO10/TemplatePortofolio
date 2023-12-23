e34bbd1ac8271edba31d309917449d20efc530b6
ATHAANGGORO10 <atha.anggoro2019@gmail.com>
2023-12-23 14:09:30 +0700

    typo

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ if (localStorage.getItem("theme") === "dark") {
 }
 
 // DarkMode-Android
-document.getElementById("darkModeToggle").addEventListener("click", function () {
+document.getElementById("darkModeToggle").addEventListener("click", () => {
      document.body.classList.toggle("dark-mode");
      if (document.body.classList.contains("dark-mode")) {
           localStorage.setItem("theme", "light");
@@ -21,12 +21,12 @@ document.getElementById("darkModeToggle").addEventListener("click", function () 
 });
 
 // DarkMode-PC dan Laptop
-document.getElementById("lightModeToggler").addEventListener("click", function () {
+document.getElementById("lightModeToggler").addEventListener("click", () => {
      document.body.classList.toggle("light-mode");
      if (document.body.classList.contains("light-mode")) {
-          localStorage.setItem("theme", "dark");
-     } else {
           localStorage.setItem("theme", "light");
+     } else {
+          localStorage.setItem("theme", "dark");
      }
      const icon = lightModeToggler.querySelector('.material-symbols-outlined');
      if (document.body.classList.contains('light-mode')) {
