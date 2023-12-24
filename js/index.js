b7469307d4c736942e540e597632ead8cc615f62
ATHAANGGORO10 <atha.anggoro2019@gmail.com>
2023-12-25 01:41:50 +0700

    typo min

--- js/index.js
+++ js/index.js
@@ -1,66 +1,61 @@
 (() => {
-     'use strict'
-     const getStoredTheme = () => localStorage.getItem('theme')
-     const setStoredTheme = theme => localStorage.setItem('theme', theme)
+     'use strict';
+     const getStoredTheme = () => localStorage.getItem('theme');
+     const setStoredTheme = theme => localStorage.setItem('theme', theme);
      const getPreferredTheme = () => {
-          const storedTheme = getStoredTheme()
+          const storedTheme = getStoredTheme();
           if (storedTheme) {
-               return storedTheme
+               return storedTheme;
           }
-          return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-     }
-
+          return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+     };
      const setTheme = theme => {
           if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-               document.documentElement.setAttribute('data-bs-theme', 'dark')
+               document.documentElement.setAttribute('data-bs-theme', 'dark');
           } else {
-               document.documentElement.setAttribute('data-bs-theme', theme)
+               document.documentElement.setAttribute('data-bs-theme', theme);
           }
+     };
+     setTheme(getPreferredTheme());
+     const updateDarkModeStatus = () => {
+          const darkModeStatus = document.getElementById('darkModeStatus');
+          if (darkModeStatus) {
+               const currentTheme = getStoredTheme() || getPreferredTheme();
+               darkModeStatus.textContent = ` (${currentTheme === 'dark' ? 'Dark' : 'Light'} Mode)`;
+          }
+     };
+     const updateIcon = () => {
+          const currentTheme = getStoredTheme() || getPreferredTheme();
+          const icon = document.getElementById('darkModeIcon');
+          if (icon) {
+               icon.classList.remove('bi-moon-stars-fill');
+               if (currentTheme === 'dark') {
+                    icon.classList.add('bi-moon-stars-fill', 'text-light');
+               } else {
+                    icon.classList.add('bi-cloud-sun-fill', 'text-dark');
+               }
+          }
+     };
+     const toggleTheme = () => {
+          const currentTheme = getStoredTheme() || getPreferredTheme();
+          const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+          setStoredTheme(newTheme);
+          setTheme(newTheme);
+          updateDarkModeStatus();
+          updateIcon();
+     };
+     const darkModeToggle = document.getElementById('darkModeToggle');
+     if (darkModeToggle) {
+          darkModeToggle.addEventListener('click', toggleTheme);
      }
-
-     setTheme(getPreferredTheme())
-     const showActiveTheme = (theme, focus = false) => {
-          const themeSwitcher = document.querySelector('#bd-theme')
-          if (!themeSwitcher) {
-               return
-          }
-          const themeSwitcherText = document.querySelector('#bd-theme-text')
-          const activeThemeIcon = document.querySelector('.theme-icon-active')
-          const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-          const iconOfActiveBtn = btnToActive.querySelector('i').dataset.themeIcon;
-          document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-               element.classList.remove('active')
-               element.setAttribute('aria-pressed', 'false')
-          })
-          btnToActive.classList.add('active')
-          btnToActive.setAttribute('aria-pressed', 'true')
-          activeThemeIcon.classList.remove(activeThemeIcon.dataset.themeIconActive)
-          activeThemeIcon.classList.add(iconOfActiveBtn)
-          activeThemeIcon.dataset.iconActive = iconOfActiveBtn
-          const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
-          themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
-          if (focus) {
-               themeSwitcher.focus()
-          }
-     }
-
      window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-          const storedTheme = getStoredTheme()
+          const storedTheme = getStoredTheme();
           if (storedTheme !== 'light' && storedTheme !== 'dark') {
-               setTheme(getPreferredTheme())
+               setTheme(getPreferredTheme());
+               updateDarkModeStatus();
+               updateIcon();
           }
-     })
-
-     window.addEventListener('DOMContentLoaded', () => {
-          showActiveTheme(getPreferredTheme())
-          document.querySelectorAll('[data-bs-theme-value]')
-               .forEach(toggle => {
-                    toggle.addEventListener('click', () => {
-                         const theme = toggle.getAttribute('data-bs-theme-value')
-                         setStoredTheme(theme)
-                         setTheme(theme)
-                         showActiveTheme(theme, true)
-                    })
-               })
-     })
-})()
+     });
+     updateDarkModeStatus();
+     updateIcon();
+})();
